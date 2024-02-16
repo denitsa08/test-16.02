5272055302039ab57589cbbf1b52d10feadec513
denica08 <denitsa88pa@gmail.com>
2024-02-16 10:38:40 +0200

    add if

--- test16.02/Program.cs
+++ test16.02/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Denica");
+Console.WriteLine("Въведи възрастта си :");
+int age = int.Parse(Console.ReadLine());
+if (age >= 18)
+    {
+    Console.WriteLine("Пълнолетен.")
+    }
+else
+    {
+    Console.WriteLine("Непълнолетен.")
+    }
+
